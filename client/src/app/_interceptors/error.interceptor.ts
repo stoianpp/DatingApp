94430f2b6283fa67accc1ab95152c97f10fe0f8f
Stoyan Penchev <stoianpp2013@gmail.com>
2020-11-08 21:51:03 +0200

    AutoMapper configured

--- client/src/app/_interceptors/error.interceptor.ts
+++ client/src/app/_interceptors/error.interceptor.ts
@@ -44,6 +44,7 @@ export class ErrorInterceptor implements HttpInterceptor {
               break;
             case 500:
               const navigationExtras: NavigationExtras = {state: {error: error.error}}
+              this.toastr.error(error.statusText === 'OK' ? 'Unauthorised' : error.statusText, error.status);
               this.router.navigateByUrl('/server-error', navigationExtras);
               break;
             default:
